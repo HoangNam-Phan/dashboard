e396e8278f100a8bf721a2d61b98514996715424
HoangNam-Phan <hoangnamphan@outlook.de>
2024-02-02 22:20:40 +0100

    added breakpoints to dashboard components

--- app/[lng]/dashboard/page.tsx
+++ app/[lng]/dashboard/page.tsx
@@ -3,7 +3,7 @@ import Card from '@/app/[lng]/components/Card';
 const apps = [
   {
     name: 'WEATHER COMPONENT',
-    customClasses: 'col-span-2',
+    customClasses: 'col-span-2 row-start-1',
   },
   {
     name: 'STOCK COMPONENT',
@@ -11,22 +11,28 @@ const apps = [
   },
   {
     name: 'CLOCK COMPONENT',
-    customClasses: 'col-start-3 row-start-1',
+    customClasses: 'row-start-3 lg:row-start-1',
   },
   {
     name: 'TODO COMPONENT',
-    customClasses: 'row-span-2 col-start-4 row-start-1',
+    customClasses: 'row-start-3 row-span-2 sm:row-start-3 lg:row-start-1',
   },
   {
     name: 'NOTES COMPONENT',
-    customClasses: 'col-start-3 row-start-2',
+    customClasses: 'row-start-4 lg:row-start-2',
   },
 ];
 
 export default function Dashboard() {
   return (
     <div className="h-full flex items-center justify-center">
-      <div className="bg-gray-100 shadow-md rounded-lg bg-white p-10 pt-24 h-full w-full grid grid-cols-4 grid-rows-2 gap-4">
+      <div
+        className={`
+        bg-gray-100 shadow-md rounded-lg bg-white p-5 sm:p-10 pt-14 sm:pt-20 h-full w-full
+        grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 
+        grid-rows-4 sm:grid-rows-4 lg:grid-rows-2 gap-5
+        `}
+      >
         {apps.map((app) => {
           return (
             <Card key={app.name} customClasses={app.customClasses}>
