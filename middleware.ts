12942f96543fb3965ca902ec6f3303e3916ed767
HoangNam-Phan <hoangnamphan@outlook.de>
2024-02-02 02:34:29 +0100

    cleaned up middleware

--- middleware.ts
+++ middleware.ts
@@ -5,68 +5,88 @@ import { fallbackLang, languages, cookieName } from './app/i18n/settings';
 
 acceptLanguage.languages(languages);
 
-function navigationGuard(req: NextRequest, lang: string | undefined) {
+function setCurentLang(req: NextRequest) {
+  let lang;
+
+  if (req.cookies.has(cookieName)) {
+    return (lang = acceptLanguage.get(req.cookies.get(cookieName)?.value));
+  }
+  if (!lang) {
+    return (lang = acceptLanguage.get(req.headers.get('Accept-Language')));
+  }
+  if (!lang) {
+    return (lang = fallbackLang);
+  }
+}
+
+function navigationGuard(req: NextRequest, lang: string | undefined | null) {
   const path = req.nextUrl.pathname;
   const authToken = req.cookies.get('token')?.value;
 
-  //to-do: fix url
-  
   if (authToken !== '1') {
-    if (!authToken && path === `/${lang}/dashboard`) {
+    if (
+      (!authToken && path === '/de/dashboard') ||
+      (!authToken && path === '/en/dashboard')
+    ) {
       return NextResponse.redirect(new URL(`/${lang}/login`, req.url));
     }
-
     if (authToken) {
       if (
-        path === `/${lang}` ||
-        path === `/${lang}/login` ||
-        path === `/${lang}/signup`
+        path === '/de' ||
+        path === '/de/login' ||
+        path === '/de/signup' ||
+        path === '/en' ||
+        path === '/en/login' ||
+        path === '/en/signup'
       ) {
         return NextResponse.redirect(new URL(`/${lang}/dashboard`, req.url));
       }
     }
   }
-  console.log(req.url)
-  return NextResponse.redirect(new URL(`/${lang}/dashboard`, req.url));
 }
 
-export function middleware(req: NextRequest) {
-  let lang;
-
-  if (req.cookies.has(cookieName)) {
-    lang = acceptLanguage.get(req.cookies.get(cookieName)?.value);
-  }
-  if (!lang) {
-    lang = acceptLanguage.get(req.headers.get('Accept-Language'));
-  }
-  if (!lang) {
-    lang = fallbackLang;
-  }
-
-  if (
-    !languages.some((lang) => req.nextUrl.pathname.startsWith(`/${lang}`)) &&
-    !req.nextUrl.pathname.startsWith('/_next')
-  ) {
-    return NextResponse.redirect(
-      new URL(`/${lang}${req.nextUrl.pathname}`, req.url)
-    );
-  }
-
+function updateLang(req: NextRequest) {
   if (req.headers.has('referer')) {
     const refererUrl = new URL(req.headers.get('referer') as string);
     const lngInReferer = languages.find((lang) =>
       refererUrl.pathname.startsWith(`/${lang}`)
     );
     const response = NextResponse.next();
-    console.log(response)
     if (lngInReferer) {
       response.cookies.set(cookieName, lngInReferer);
-      const newLang = response.cookies.get(cookieName)?.value
-      navigationGuard(req, newLang);
+    }
+    const newLang = response.cookies.get(cookieName)?.value;
+    const redirect = navigationGuard(req, newLang);
+
+    if (redirect) {
+      return redirect;
     }
 
     return response;
   }
+}
+
+export function middleware(req: NextRequest) {
+  const currentLang = setCurentLang(req);
+
+  if (
+    !languages.some((lang) => req.nextUrl.pathname.startsWith(`/${lang}`)) &&
+    !req.nextUrl.pathname.startsWith('/_next')
+  ) {
+    return NextResponse.redirect(
+      new URL(`/${currentLang}${req.nextUrl.pathname}`, req.url)
+    );
+  }
+
+  const setLang = updateLang(req);
+  if (setLang) return setLang;
+
+  const redirect = navigationGuard(req, currentLang);
+  if (redirect) return redirect;
 
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
